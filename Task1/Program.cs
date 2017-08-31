f4dfee297c6a876a37c58fc342d8f6f4f9de65d8
miha2158 <miha.vanya@gmail.com>
2017-09-01 00:37:43 +0500

    Делаю алгоритм проще, вдруг будет быстрее работать

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,72 +5,134 @@ namespace Task1
 {
     class Program
     {
-        static long Factorial(long num)
+        static int find(string s)
         {
-            if (num == 0)
-                return 1;
-            return num * Factorial(num - 1);
+            string r = string.Empty;
+            foreach (char c in s)
+                if (!r.Contains(c.ToString()))
+                    r = r + c;
+            return factorial(s.Length) / factorial(s.Length - r.Length) + 1;
+        }
+        static int factorial(int num)
+        {
+            switch (num)
+            {
+                case 0: case 1:
+                    return 1;
+                default:
+                    return num * factorial(num - 1);
+            }
         }
         static string[] MakeVariations(string s)
         {
             if (s.Length <= 1)
                 return new[] { s };
             
-            var result = new string[Factorial(s.Length)];
+            var result = new string[find(s)];
             for (int i = 0,k = 0; i < s.Length; i++)
             {
                 string s1 = string.Empty;
                 for (int j = 0; j < s.Length; j++)
                     if(j != i)
                         s1 = s1 + s[j];
+
+                bool skip = false;
+                for (int j = 0; j < result.Length && !string.IsNullOrEmpty(result[j]) && !skip; j++)
+                    if (s[i] + s1 == result[j])
+                        skip = true;
+                if(skip)
+                    continue;
+
                 var res1 = MakeVariations(s1);
+                for (int index = 0; index < res1.Length; index++)
+                {
+                    string temp = string.Empty;
+                    if (!string.IsNullOrEmpty(res1[index]))
+                        temp = s[i] + res1[index];
+                    else
+                        break;
 
-                foreach (string s2 in res1)
-                    if (Array.IndexOf(result, s[i] + s2) == -1)
-                        result[k++] = s[i] + s2;
+                    for (int j = 0; j < result.Length; j++)
+                    {
+                        if (result[j] == temp)
+                            break;
+                        if (string.IsNullOrEmpty(result[j]))
+                        {
+                            result[k++] = temp;
+                            break;
+                        }
+                    }
+                }
             }
-
-            {
-                int size = 0;
-                foreach (string s1 in result)
-                    if (!string.IsNullOrEmpty(s1))
-                        size++;
-                var temp = new string[size];
-
-                int i = 0;
-                foreach (var s1 in result)
-                    if (!string.IsNullOrEmpty(s1))
-                        temp[i++] = s1;
-                result = temp;
-            }
-
             return result;
         }
+
+        static string[] DumbMakeVariations(string s)
+        {
+            if (s.Length <= 1)
+                return new[] { s };
+
+            var result = new string[factorial(s.Length)];
+
+            for (int i = 0, k = 0; i < s.Length; i++)
+            {
+                string s1 = string.Empty;
+                for (int j = 0; j < s.Length; j++)
+                    if (j != i)
+                        s1 = s1 + s[j];
+
+
+                var res1 = MakeVariations(s1);
+                foreach (string str in res1)
+                    result[k++] = s[i] + str;
+            }
+            return result;
+        }
+        static string[] RemoveDuplicates(string[] s)
+        {
+            var result = new string[find(s[0])];
+            for (int i = 0,k = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < result.Length; j++)
+                {
+                    if (s[i] == result[j])
+                        break;
+                    if (!string.IsNullOrEmpty(result[j]))
+                        continue;
+                    result[k++] = s[i];
+                    break;
+                }
+            }
+            return result;
+        }
+
 
         static void Main(string[] args)
         {
             string input;
+            using (var fs = new FileStream("INPUT.TXT", FileMode.Open))
             {
-                var fs1 = new FileStream("INPUT.TXT", FileMode.OpenOrCreate);
-                var sr = new StreamReader(fs1);
-                input = sr.ReadToEnd();
-                sr.Close();
-                fs1.Close();
+                using (var sr = new StreamReader(fs))
+                {
+                    input = sr.ReadLine();
+                }
             }
-            //input = input.Replace("\n", "").Replace("\r", "").Replace(" ", "");
 
-            string[] outputArray = MakeVariations(input);
             string output = string.Empty;
-            foreach (string s in outputArray)
-                output = output + s.Trim() + Environment.NewLine;
-            output = output.Trim();
-
             {
-                var fs2 = new FileStream("OUTPUT.TXT", FileMode.Create);
-                var sw = new StreamWriter(fs2);
-                sw.Write(output);
-                sw.Close();
-                fs2.Close();
+                //var outputArray = MakeVariations(input);
+                var outputArray = RemoveDuplicates(DumbMakeVariations(input));
+                for (int i = 0; i < outputArray.Length && !string.IsNullOrEmpty(outputArray[i]); i++)
+                        output = output + outputArray[i] + Environment.NewLine;
+            }
+
+            using (var fs = new FileStream("OUTPUT.TXT", FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    for (int i = 0; i < output.Length - 1; i++)
+                        sw.Write(output[i]);
+                }
             }
         }
     }
